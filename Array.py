f8257e0f790cbc5f7bfdebd19ff522ee83343a4f
suhana783 <suhanachauhan133@gmail.com>
2025-08-08 16:43:05 +0530

    solved new question

--- Array.py
+++ Array.py
@@ -42,15 +42,26 @@
 
 def sort_array(arr1,arr2):
     arr3 = []
-    for i in range(len(arr1)):
-        for j in range(len(arr2)):
-            if arr1[i] > arr2[j]:
-                arr3.append(arr2[j])
-            else:
-                arr3.append(arr1[i])
-                
-               
-    return(arr3)
+    i = 0
+    j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            arr3.append(arr1[i])
+            i += 1
+        else:
+            arr3.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        arr3.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        arr3.append(arr2[j])
+        j += 1
+
+    return arr3
+  
 
 arr1 = [1,2,3]
 arr2 = [4,5,6]
